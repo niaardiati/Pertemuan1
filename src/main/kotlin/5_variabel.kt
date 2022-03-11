ea597f60bcce4bf07b7a967b23e77f0ba44b1887
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-11 16:29:40 +0700

    Percobaan Variabel

---
+++ src/main/kotlin/5_variabel.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //Tipe dinamis
+var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    //Tipe statis
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
