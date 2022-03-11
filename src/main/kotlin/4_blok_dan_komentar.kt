b840c052171e2871f1bea4c46b3b8f77b06467ff
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-11 16:22:20 +0700

    Percobaan blok dan komentar

---
+++ src/main/kotlin/4_blok_dan_komentar.kt
@@ -0,0 +1,16 @@
+//Blok
+fun main() {
+    //blok ditandai dengan tanda buka dan tutup kurung kurawal
+    println("Hello, ini Program Pertama Saya di Kotlin!")
+}
+
+//Komentar
+// Contoh komentar 1 baris
+/*
+Contoh komentar 1 baris atau lebih
+ */
+/**
+ * dokumentasi kode program
+ */
+
+
